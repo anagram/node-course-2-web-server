8d53dff6aa2e0365118a2a9b0b52201d60715420
Anne Graham <anagramfx@gmail.com>
2017-03-07 10:47:57 -0800

    add a maintain page and change order of express middleware calls

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// the following bypasses everything after it from rendering.
+// -----  since next never gets called
+
+// app.use((req, res, next) => {
+//   res.render('maintain.hbs', {
+//     pageTitle: "We'll be right back"
+//   })
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
